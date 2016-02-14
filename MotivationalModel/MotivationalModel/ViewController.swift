5f34b0e46f7731e00982a421ba08205a56a15cf9
Andy Malik <me@andymalik.com>
2016-02-13 22:04:31 -0800

    Intro Storyboard layout created. Linked storyboard with class, and added template functionality for buttons.

--- MotivationalModel/MotivationalModel/ViewController.swift
+++ MotivationalModel/MotivationalModel/ViewController.swift
@@ -8,15 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IntroViewController: UIViewController {
 
+    // Storyboard Items
+    
+    @IBOutlet weak var introBody: UILabel!
+    @IBOutlet weak var nobLabel: UILabel!
+    @IBOutlet weak var nobInputField: UITextField!
+
+    
+    //UIViewController inheritance functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Introduction"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    // Button Functions
+    @IBAction func menuButton(sender: UIBarButtonItem) {
+        let label = "  MENU BUTTON PRESSED  "
+        self.introBody.text = "\(self.introBody.text!) \(label)"
+        self.nobLabel.text = label
+        self.nobInputField.placeholder = label
+    }
+    
+    @IBAction func viewPropButton(sender: UIButton) {
+        let label = "  VIEW PROP BUTTON PRESSED  "
+        self.introBody.text = "\(self.introBody.text!) \(label)"
+        self.nobLabel.text = label
+        self.nobInputField.placeholder = label
+    }
+    
 }
 
