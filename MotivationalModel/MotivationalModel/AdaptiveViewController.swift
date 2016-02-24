a9f566f15e2327a0e633560066b2e3f1ca49b601
Andy Malik <me@andymalik.com>
2016-02-24 11:31:48 -0800

    Added in new view controller and a class for the view controller.

---
+++ MotivationalModel/MotivationalModel/AdaptiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdaptiveViewController.swift
+//  MotivationalModel
+//
+//  Created by Andy Malik on 2/24/16.
+//  Copyright © 2016 VanguardStrategiesInc. All rights reserved.
+//
+
+import UIKit
+
+class AdaptiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
