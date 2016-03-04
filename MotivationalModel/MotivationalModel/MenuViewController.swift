aae5e456ae97224d1d1e19dd825abb0d05b8c048
Andy Malik <me@andymalik.com>
2016-03-03 22:36:30 -0800

    Printing is now functional.

--- MotivationalModel/MotivationalModel/MenuViewController.swift
+++ MotivationalModel/MotivationalModel/MenuViewController.swift
@@ -43,13 +43,52 @@ class MenuViewController: UIViewController, MFMailComposeViewControllerDelegate 
         switch (buttonName) {
             //Home is omitted since we're just using an unwind segue
         case "Go To": self.performSegueWithIdentifier("MenuToGotoSegue", sender: nil)
-        case "E-mail": confirmUserName("email")
-        case "Print": makePDF("") // printWithPDF()
+        case "E-mail": confirmBusinessName("email")
+        case "Print": confirmBusinessName("print")
         case "About": self.performSegueWithIdentifier("MenuToAboutSegue", sender: nil)
         case "Close": dismissViewControllerAnimated(true, completion: nil)
         default: print("Default was hit?")
         }
     }
+    
+    func confirmBusinessName(destination: String) {
+        if let businessName = RoomsCache.shared.findRoom(kHome).userText {
+            if businessName == "" {
+               
+                let namePopup = UIAlertController(title: "Business Name", message: "Please put in a name for your business on the Home page.", preferredStyle: .Alert)
+                namePopup.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                self.presentViewController(namePopup, animated: true, completion: nil)
+                
+            } else {
+                self.confirmEmptyFields(destination)
+            }
+        }
+    }
+    
+    func confirmEmptyFields(destination: String) {
+        var message = ""
+        var isSafe = true
+        for currRoom in RoomsCache.shared.rooms {
+            if let thisRoomUserInput = currRoom.userText where thisRoomUserInput.isEmpty {
+                message = "\(message)\n\(currRoom.title)"
+                isSafe = false
+            }
+        }
+        
+        let emptyFieldPopup = UIAlertController(title: "Are you sure?", message: "The following fields are empty:\n\(message)", preferredStyle: .Alert)
+        emptyFieldPopup.addAction(UIAlertAction(title: "Continue", style: .Default, handler: { (action) -> Void in
+            self.confirmUserName(destination)
+        }))
+        emptyFieldPopup.addAction(UIAlertAction(title: "Cancel", style: .Destructive, handler: nil))
+        
+        if !isSafe {
+            presentViewController(emptyFieldPopup, animated: true, completion: nil)
+        } else {
+            self.confirmUserName(destination)
+        }
+        
+    }
+    
     
     func confirmUserName(destination: String) {
         if RoomsCache.shared.username == "" {
@@ -57,7 +96,7 @@ class MenuViewController: UIViewController, MFMailComposeViewControllerDelegate 
         } else {
             let namePopup = UIAlertController(title: "Name", message: "Is your name \(RoomsCache.shared.username)?", preferredStyle: .Alert)
             namePopup.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action) -> Void in
-                self.confirmBusinessName(destination)
+                self.makePDF(destination)
             }))
             namePopup.addAction(UIAlertAction(title: "No", style: .Default, handler: { (action) -> Void in
                 self.getUserName(destination)
@@ -76,7 +115,7 @@ class MenuViewController: UIViewController, MFMailComposeViewControllerDelegate 
             guard let name = nameTextField.text else { fatalError("11") }
             RoomsCache.shared.username = name
             NSKeyedArchiver.archiveRootObject(RoomsCache.shared.username, toFile: String.archivePath("username"))
-            self.confirmBusinessName(destination)
+            self.makePDF(destination)
         }
         nameAction.enabled = false
         
@@ -92,18 +131,6 @@ class MenuViewController: UIViewController, MFMailComposeViewControllerDelegate 
         namePopup.addAction(UIAlertAction(title: "Cancel", style: .Default, handler: nil))
         
         self.presentViewController(namePopup, animated: true, completion: nil)
-    }
-    
-    func confirmBusinessName(destination: String) {
-        if let businessName = RoomsCache.shared.findRoom(kHome).userText {
-            if businessName == "" {
-                let namePopup = UIAlertController(title: "Business Name", message: "Please put in a name for your business on the Home page.", preferredStyle: .Alert)
-                namePopup.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
-                self.presentViewController(namePopup, animated: true, completion: nil)
-            } else {
-                self.makePDF(destination)
-            }
-        }
     }
     
     func confirmMissingSections(destination: String) {
@@ -148,7 +175,7 @@ class MenuViewController: UIViewController, MFMailComposeViewControllerDelegate 
                 noEmailFound()
             }
             
-        }
+        } else { fatalError("20319") }
     }
     
     func noEmailFound() {
@@ -179,12 +206,37 @@ class MenuViewController: UIViewController, MFMailComposeViewControllerDelegate 
         
     }
     
-    //MARK: Print coming soon.
+    //MARK: AirPrint
     func printWithPDF() {
-        let namePopup = UIAlertController(title: "Coming Soon!", message: "", preferredStyle: .Alert)
-        namePopup.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
-        self.presentViewController(namePopup, animated: true, completion: nil)
+        
+        
+        if let businessName = RoomsCache.shared.findRoom(kHome).userText {
+            
+            let dashedBusinessName = businessName.stringByReplacingOccurrencesOfString(" ", withString: "-")
+            let fileName = "\(dashedBusinessName)-Business-Model.pdf"
+            let path: NSArray = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+            let documentDirectory = path.objectAtIndex(0)
+            let pdfPathWithFileName = documentDirectory.stringByAppendingPathComponent(fileName)
+            guard let pdfDATA = NSData(contentsOfFile: pdfPathWithFileName) else { fatalError("SO CLOSE") }
+            
+            if UIPrintInteractionController.isPrintingAvailable() && UIPrintInteractionController.canPrintData(pdfDATA) {
+                
+                let printController = UIPrintInteractionController.sharedPrintController()
+                
+                let printInfo = UIPrintInfo(dictionary: nil)
+                printInfo.outputType = .Grayscale
+                printInfo.jobName = "\(fileName)"
+                printController.printInfo = printInfo
+                printController.showsPageRange = true
+                printController.printingItem = pdfDATA
+                
+                printController.presentAnimated(true, completionHandler: nil)
+            
+            }
+            
+        }
     }
+    
     
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self)
